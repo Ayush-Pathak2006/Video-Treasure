d5754fd8584fd15d640071d0d08ae235753a0588
Ayush <ayushpathak13022006@gmail.com>
2025-08-30 01:52:58 +0530

    CORS issue tackled

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,8 +4,9 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: video-treasure.vercel.app,
+    credentials: true,
+    optionsSuccessStatus: 200
 }));
 
 app.use(express.json({ limit: "16kb" }));
