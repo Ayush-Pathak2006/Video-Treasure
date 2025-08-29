807638f7a62889272a83f06a70b809400e2545e5
Ayush <ayushpathak13022006@gmail.com>
2025-08-30 03:36:18 +0530

    debugging db

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,6 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
+        console.log("Attempting to connect with MONGODB_URI:", process.env.MONGODB_URI);
+
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`\n✅ MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
