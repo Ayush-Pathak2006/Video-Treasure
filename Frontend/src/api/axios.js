baccb6e2f2e7a20febcf2d0d011bd2d725a8674d
Ayush Pathak <ayushpathak13022006@gmail.com>
2026-03-31 17:23:00 +0530

    Validating deployment issue

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://video-treasure-backend.onrender.com/api',
+  baseURL: 'https://video-treasure-backend.onrender.com',
   withCredentials: true,
 });
 
